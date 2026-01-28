bc1e23ff64a54fb6a02367f6c802a87b5fc19804
Ashisha Justin <ashisha@gmail.com>
2026-01-28 22:22:31 +0530

    Added electricity bill calculator program

---
+++ bill_calculator.py
@@ -0,0 +1,20 @@
+def calculate_bill(units):
+    if units <= 100:
+        amount = units * 1.5
+    elif units <= 200:
+        amount = 100 * 1.5 + (units - 100) * 2.5
+    elif units <= 300:
+        amount = 100 * 1.5 + 100 * 2.5 + (units - 200) * 4
+    else:
+        amount = 100 * 1.5 + 100 * 2.5 + 100 * 4 + (units - 300) * 5
+
+    return amount
+
+
+try:
+    units = int(input("Enter electricity units consumed: "))
+    bill = calculate_bill(units)
+    print(f"Total Electricity Bill: ₹{bill}")
+    print("\nThank you for using Smart Electricity Billing system")
+except ValueError:
+    print("Please enter a valid number.")
